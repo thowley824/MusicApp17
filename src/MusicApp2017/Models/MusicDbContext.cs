d58bab6c4adff37221a2517666f4a98b516f19bd
thowley824 <howleyt@go.stockton.edu>
2017-04-12 06:12:49 -0400

    Music App: Lab 5 Complete

---
+++ src/MusicApp2017/Models/MusicDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicApp2017.Models
+{
+    public class MusicDbContext : DbContext
+    {
+
+        public MusicDbContext(DbContextOptions<MusicDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
+
